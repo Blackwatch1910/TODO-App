cadc2021a5fb9b685695c02a5df82c9a85bfed58
Blackwatch1910 <dcoolguydev@gmail.com>
2019-09-10 01:18:19 +0530

    Adding Update and Delete Option

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var list = document.getElementById('todo-list');
 var inputBox = document.getElementById('todo-input');
 var btnAdd = document.getElementById('add-item');
+var btnUpdate = document.getElementById('update-item');
+var btnRemove = document.getElementById('remove-item');
+
 var currentInputVal = '';
 
 inputBox.addEventListener('input', function(e) {
@@ -12,14 +15,20 @@ inputBox.addEventListener('keyup', function(e) {
 		//alert("Enter Key is Pressed");
 		addNewList();
 	}
-})
+});
 
-function addNewList() {
-	if(currentInputVal !== undefined && currentInputVal !== null && currentInputVal !== '') {
-		var newListElement = document.createElement('li');
+function createNewNode() {
+	var newListElement = document.createElement('li');
 	var textNode = document.createTextNode(currentInputVal)
 	newListElement.appendChild(textNode);
 	newListElement.id = 'item' + (list.childElementCount + 1);
+
+	return newListElement;
+}
+
+function addNewList() {
+	if(currentInputVal !== undefined && currentInputVal !== null && currentInputVal !== '') {
+		var newListElement = createNewNode();
 	
 	list.appendChild(newListElement);
 	
@@ -32,3 +41,20 @@ function addNewList() {
 }
 
 btnAdd.addEventListener('click', addNewList);
+
+btnUpdate.addEventListener('click', function() {
+	//alert('Update Item was clicked');
+	var firstElement = list.firstElementChild;
+	var newListElement = createNewNode();
+
+	list.replaceChild(newListElement, firstElement);
+})
+
+btnRemove.addEventListener('click', function() {
+	var firstElement = list.firstElementChild;
+	//alert('Delete was clicked')
+	if(firstElement === undefined || firstElement === null || firstElement === '') {
+		alert('No Items to Delete');
+	}
+	list.removeChild(firstElement);
+})
